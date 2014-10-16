1aa9aa72aae821971caca91d854a607f2704b863
Nick Podratz <n1ck@MacBook-13.local>
2014-10-16 22:23:50 +0200

    Initial Commit

---
+++ Rhetorica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rhetorica
+//
+//  Created by Nick Podratz on 16.10.14.
+//  Copyright (c) 2014 Nick Podratz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
