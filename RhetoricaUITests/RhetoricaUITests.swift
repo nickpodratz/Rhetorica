e8b3a566e25738f92fd460013b3302802c06d7fa
Nick Morten Podratz <Nick@Podratz.de>
2016-04-19 17:02:30 +0200

    UITest for the screenshot creation-tool 'Snapshot'

--- RhetoricaUITests/RhetoricaUITests.swift
+++ RhetoricaUITests/RhetoricaUITests.swift
@@ -39,39 +39,80 @@ class RhetoricaUITests: XCTestCase {
     }
     
     func testTakeScreenshots() {
+        
+        // Setup app
         let app = XCUIApplication()
+//        app.launchEnvironment = [ "UITest": "1" ]
+//        app.launchArguments = ["testMode"]
+//        // setLanguage(app)
+//        setupSnapshot(app)
         
-        // Select right device list
-        app.navigationBars.elementBoundByIndex(0).buttons["list"].tap()
-        app.tables.staticTexts["All Devices"].tap()
-        app.navigationBars["Extent"].buttons["Done"].tap()
-       
-        snapshot("0Launch")
+        let mainVC_tableView = app.tables.elementBoundByIndex(0)
+        let mainVC_listButton = app.buttons.matchingIdentifier("MainVC_ListMenuButton").element
+
+        // Choose device for detailVC (for iPad where detailVC persists)
+        mainVC_tableView.staticTexts["Alliteration"].tap()
         
-//        let tableView = app.tables.elementBoundByIndex(0)
-//        tableView
-//        
-//        let cell = app.staticTexts["Allusion"]
-//        let start = cell.coordinateWithNormalizedOffset(CGVectorMake(0, 0))
-//        let finish = cell.coordinateWithNormalizedOffset(CGVectorMake(0, 6))
-//        start.pressForDuration(0, thenDragToCoordinate: finish)
-//
-//        app.scrollViews.otherElements.icons["Rhetorica"].tap()
-//        
-//
-//        let tablesQuery = XCUIApplication().tables
-//        tablesQuery.childrenMatchingType(.Other).matchingIdentifier("table index").elementBoundByIndex(0).tap()
-//        tablesQuery.cells.containingType(.StaticText, identifier:"Salutatio").childrenMatchingType(.StaticText).matchingIdentifier("Salutatio").elementBoundByIndex(0).tap()
-//        tablesQuery.cells.containingType(.StaticText, identifier:"Repetition").childrenMatchingType(.StaticText).matchingIdentifier("Repetition").elementBoundByIndex(0).tap()
-//        tablesQuery.staticTexts["Rhetorical Question"].tap()
-//        
-//        snapshot("1detailVC")
-//
-//        let hyperboleStaticText = app.tables.staticTexts["Hyperbole"]
-//        hyperboleStaticText.tap()
-//        hyperboleStaticText.tap()
-//        app.childrenMatchingType(.Window).elementBoundByIndex(1).childrenMatchingType(.Other).elementBoundByIndex(1).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(0).childrenMatchingType(.ScrollView).element.tap()
-//        app.typeText("")
-    
+        // Pop back to mainVC if device is NOT iPad
+        if !mainVC_listButton.exists || !mainVC_listButton.hittable {
+            app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(0).tap()
+        }
+        
+        // Present ListVC
+        mainVC_listButton.tap() //MainVC_NavigationBar
+        
+        // Select AllDevices list
+        let listVC_tableView = app.tables.elementBoundByIndex(0)
+        let listVC_allDevicesCell = listVC_tableView.cells.matchingIdentifier("ListVC_AllDevicesCell").element
+        listVC_allDevicesCell.tap()
+        
+        snapshot("5Settings")
+
+        // Pop back to mainVC
+        let listVC_navigationBar = app.navigationBars.matchingIdentifier("listVC_NavigationBar").element
+        let listVC_doneButton = listVC_navigationBar.buttons.matchingIdentifier("ListVC_DoneButton").element
+        listVC_doneButton.tap()
+        
+        listVC_tableView.swipeUp()
+        listVC_tableView.swipeUp()
+        listVC_tableView.swipeUp()
+        listVC_tableView.swipeUp()
+        
+        snapshot("4TableView")
+        
+        // Choose device for detailVC snapshot
+        if listVC_tableView.staticTexts["Telling Name"].exists {
+            listVC_tableView.staticTexts["Telling Name"].tap()
+
+        } else if listVC_tableView.staticTexts["Euphemismus"].exists {
+            listVC_tableView.staticTexts["Euphemismus"].tap()
+
+        } else {
+            listVC_tableView.staticTexts["Inversion"].tap()
+        }
+        
+        snapshot("1DetailView")
+        
+        let detailVC_navigationBar = app.navigationBars.matchingIdentifier("DetailVC_NavigationBar").element
+        if !detailVC_navigationBar.exists || !detailVC_navigationBar.hittable {
+            // Pop back to mainVC
+            app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(0).tap()
+        }
+        
+        // Show QuizVC
+        let mainVC_quizButton = app.buttons.matchingIdentifier("MainVC_QuizMenuButton").element
+        mainVC_quizButton.tap()
+        
+        sleep(3)
+        
+        let quizRightAnswerButton = app.buttons.matchingIdentifier("QuizVC_AnswerButton").elementBoundByIndex(1)
+        quizRightAnswerButton.tap()
+        NSThread.sleepForTimeInterval(0.4)
+        snapshot("2Quiz")
+        
+        sleep(2)
+        
+        snapshot("3UserStats")
+
     }
 }
