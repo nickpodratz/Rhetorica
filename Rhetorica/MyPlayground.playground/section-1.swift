82ced74cbb6dafcfe47d08c4aa9a90ab953b73c6
Nick Morten Podratz <Nick@Podratz.de>
2016-05-16 16:30:53 +0200

    Meta Data for App Store added into fastlane files.

--- Rhetorica/MyPlayground.playground/section-1.swift
+++ Rhetorica/MyPlayground.playground/section-1.swift
@@ -23,4 +23,4 @@ import Foundation
 "rhetoric,rhetorical,device,means,literature,analysis,analyse,essay,english,class,texts,SAT,german".characters.count
 "Rhetorik,rhetorische,Germanistik,Lyrik,Analyse,Erörterung,Literatur,Abi,Abitur,Deutsch,Unterricht".characters.count
 
-"The most effective way of learning is playing. Rhetorica helps you remember the terms and usecases of stylistic devices playfully and avoid the annoyance of gazing at boring papersheets for hours. It's the tool you have been looking for if you write analyses as school, study literature or simply want to stand out with your in-depth knowledge of stylistic devices.".characters.count
+"Rhetorica helps you remember the terms and usecases of stylistic devices playfully and avoid the annoyance of gazing at papersheets for hours. It's the tool you have been looking for if you write analyses as school, study literature or simply want to stand out with a comprehensive intellectual background knowledge.".characters.count
