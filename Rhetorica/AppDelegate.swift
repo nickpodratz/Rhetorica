78c6180b371d948a985741b49980094614dd57df
Nick Morten Podratz <Nick@Podratz.de>
2016-06-08 22:06:09 +0200

    Added  'masterVC.tableView.reloadData()' to applicationWillEnterForeground function in order to prevent a layout-bug occurring when the app wakes up.

--- Rhetorica/AppDelegate.swift
+++ Rhetorica/AppDelegate.swift
@@ -52,6 +52,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        guard let splitVC = self.window?.rootViewController as? UISplitViewController,
+            let firstNavigationVC = splitVC.viewControllers.first as? UINavigationController,
+            let masterVC = firstNavigationVC.viewControllers.first as? MasterViewController else {
+                print("Can't downcast neccessary viewControllers for quick actions.")
+                return
+        }
+
+        // prevents a layout-bug with the noDataCell occurring when the app wakes up.
+        masterVC.tableView.reloadData()
     }
     
     func applicationDidBecomeActive(application: UIApplication) {
