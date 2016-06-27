37268044cc9cd5a1fe7cfbbb1262c3991e8242a0
Nick Morten Podratz <Nick@Podratz.de>
2016-06-27 22:34:16 +0200

    Recovered NoListView and uses it instead of cell. Increased app version number to 1.3.1.

--- Rhetorica/MyPlayground.playground/section-1.swift
+++ Rhetorica/MyPlayground.playground/section-1.swift
@@ -24,3 +24,10 @@ import Foundation
 "Rhetorik,rhetorische,Germanistik,Lyrik,Analyse,Erörterung,Literatur,Abi,Abitur,Deutsch,Unterricht".characters.count
 
 "Rhetorica helps you remember the terms and usecases of stylistic devices playfully and avoid the annoyance of gazing at papersheets for hours. It's the tool you have been looking for if you write analyses as school, study literature or simply want to stand out with a comprehensive intellectual background knowledge.".characters.count
+
+
+let numbers = [1, 35, 632, 6423, 46, 235, 5]
+
+for number in 0.stride(to: 0, by: 0.5) {
+    print(number)
+}
